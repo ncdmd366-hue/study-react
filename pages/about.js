eb37354e8b0fb54e0ad8a314b707e7d95ef73e13
Akizuki_Taiki <ncdmd366@icloud.com>
2026-01-27 15:25:14 +0900

    メイン部分のコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Links from '../components/Links'
 import { Footer } from '../components/Footer'
-import Headline from '../components/Headline'
+import { Main } from '../components/Main'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,13 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Headline page="about" />
-        <Links />
-      </main>
+      <Main page="about" />
 
       <Footer />
     </div>
   )
 }
+
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
-import Links from '../components/Links'
-import Headline from '../components/Headline'
+import { Main } from '../components/Main'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,13 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Headline page="index" />
-        <Links />
-      </main>
-
+      <Main page="index" />
       <Footer />
 
     </div>
   )
 }
+
