26a1c5c71407925cee0638701f829d9a10459df2
Akizuki_Taiki <ncdmd366@icloud.com>
2026-01-27 16:03:02 +0900

    css moduleによる管理

---
+++ components/Headline/Headline.js
@@ -0,0 +1,14 @@
+import styles from './Headline.module.css'
+
+export function Headline(props) {
+    return (
+        <>
+            <h1 className={styles.title}>{props.page} page</h1>
+
+            <p className={styles.description}>
+                Get started by editing{' '}
+                {props.children}
+            </p>
+        </>
+    )
+}
