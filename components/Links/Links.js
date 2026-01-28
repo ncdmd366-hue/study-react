6dea3e45fc2f4885e48eae70f62d50aea9f60ef7
Akizuki_Taiki <ncdmd366@icloud.com>
2026-01-28 11:57:00 +0900

    Linksのリファクタリング

--- components/Links/Links.js
+++ components/Links/Links.js
@@ -1,35 +1,17 @@
 import styles from './Links.module.css'
+import ITEMS from '../../data/LinkItems'
 
 export function Links() {
     return (
         <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-                <h3>Documentation &rarr;</h3>
-                <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-                <h3>Learn &rarr;</h3>
-                <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-                href="https://github.com/vercel/next.js/tree/master/examples"
-                className={styles.card}
-            >
-                <h3>Examples &rarr;</h3>
-                <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                className={styles.card}
-            >
-                <h3>Deploy &rarr;</h3>
-                <p>
-                    Instantly deploy your Next.js site to a public URL with Vercel.
-                </p>
-            </a>
+            {ITEMS.map((item) => {
+                return (
+                    <a href={item.href} key={item.href} className={styles.card}>
+                        <h3>{item.title}</h3>
+                        <p>{item.discription}</p>
+                    </a>
+                )
+            })}
         </div>
     )
 }
---
+++ data/LinkItems.js
@@ -0,0 +1,23 @@
+const ITEMS = [
+    {
+        href: "https://nextjs.org/docs",
+        title: "Documentation →",
+        discription: "Find in-depth information about Next.js features and API."
+    },
+    {
+        href: "https://nextjs.org/learn",
+        title: "Learn →",
+        discription: "Learn about Next.js in an interactive course with quizzes!"
+    },
+    {
+        href: "https://github.com/vercel/next.js/tree/master/examples",
+        title: "Examples →",
+        discription: "Discover and deploy boilerplate example Next.js projects."
+    },
+    {
+        href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+        title: "Deploy →",
+        discription: "Instantly deploy your Next.js site to a public URL with Vercel."
+    }
+]
+export default ITEMS;
