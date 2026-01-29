278dc143fcb800acc96cb07f548a3d30467a7f15
Akizuki_Taiki <ncdmd366@icloud.com>
2026-01-29 09:50:52 +0900

    フックス、三項演算子の導入

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,30 +1,41 @@
 import styles from 'src/components/Main/Main.module.css'
 import { Links } from 'src/components/Links'
 import { Headline } from 'src/components/Headline'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export function Main(props) {
 
-    const handleClick = useCallback((e) => {
-        e.preventDefault();
-        alert("クリックされました。");
-    }, [])
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState('hello');
+    const [isShown, setIsShown] = useState(true);
 
-    useEffect((() => {
-        // 初回表示（マウント）時の処理（Mainの初回利用時）
-        document.body.style.backgroundColor = "lightblue";
-        // アンマウント時の処理
-        return () => {
-            document.body.style.backgroundColor = "";
+    const handleDisplay = () => setIsShown(prev => !prev);
+    const handleClick = () => setCount(count + 1);
+    const handleChange = (e) => {
+        if (e.target.value.length > 10) {
+            // 5文字を超えたらアラートを出して、set Textをスキップ
+            alert("10文字以内にしてください");
+            return;
         }
-    }), [])
+        setText(e.target.value);
+    }
+
+    useEffect(() => {
+        if (count > 5) {
+            setCount(0);
+        }
+    }, [count])
 
     return (
         <main className={styles.main}>
+            <button onClick={handleDisplay}>{isShown ? 'ボタン表示' : 'ボタン非表示'}</button>
+            {isShown ? <button onClick={handleClick}>カウント：{count}</button> : ""}
+            <br />
+            <input value={text} onChange={handleChange} />
+            <p>Your type is {text}</p>
             <Headline page={props.page}>
                 <code className={styles.code}>pages/{props.page}.js</code>
             </Headline>
-            <a href="/about" onClick={handleClick}>ボタン</a>
             <Links />
         </main>
     )
