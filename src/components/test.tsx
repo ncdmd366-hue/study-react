8fc85d3870bd9e8c08cfa069f78f18c1edc8c9ea
ncdmd366-hue <ncdmd366@icloud.com>
2026-05-28 17:16:07 +0900

    test.tsx を更新

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,11 +1,20 @@
-CREATE DATABASE EDMS;
+-- もし途中まで作成されていたら削除（エラーが出ても無視してOK）
+USE EDMS;
+DROP USER IF EXISTS edms_admin;
 GO
-CREATE LOGIN edms_admin WITH PASSWORD = 'edms_admin';
+USE master;
+DROP LOGIN IF EXISTS edms_admin;
 GO
+
+-- パスワードを強くして再作成
+CREATE LOGIN edms_admin WITH PASSWORD = 'Edms_Admin1!';
+GO
+
 USE EDMS;
 GO
 CREATE USER edms_admin FOR LOGIN edms_admin;
 GO
+
 ALTER ROLE db_ddladmin ADD MEMBER edms_admin;
 ALTER ROLE db_datareader ADD MEMBER edms_admin;
 ALTER ROLE db_datawriter ADD MEMBER edms_admin;
